4685e2784794a5808eefd21f8c13ef8406b30bb2
juttayaya <admin@apps.jirawat.com>
2023-11-06 12:23:02 -0500

    GHA hello-world-app

--- nextjs/hello-world-app/pages/about.tsx
+++ nextjs/hello-world-app/pages/about.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function AboutPage() {
-  return <div>About us</div>
+  return (
+      <div>About us
+        <p />
+        <Link href="/img">Images</Link>
+      </div>
+  )
 }
--- nextjs/hello-world-app/pages/img/index.tsx
+++ nextjs/hello-world-app/pages/img/index.tsx
@@ -1,3 +1,3 @@
 export default function DayPage() {
-  return <div>Hello Day</div>
+  return <div>Images</div>
 }
