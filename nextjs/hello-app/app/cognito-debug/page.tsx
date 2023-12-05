f86cc3d7e6804b639502556e95bb2b18797e14d5
juttayaya <admin@apps.jirawat.com>
2023-12-05 16:45:12 -0500

    Cognito debug

---
+++ nextjs/hello-app/app/cognito-debug/page.tsx
@@ -0,0 +1,30 @@
+import { Buffer } from "buffer"
+import { headers } from 'next/headers'
+
+export default function Page() {
+    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html    const headersList = headers()
+
+    // Step 1: Get the key id from JWT headers (the kid field)
+    const headersList = headers()
+    const encodedJwt = headersList.get('x-amzn-oidc-data')
+    const jwtEncodedTokenList = encodedJwt.split('.')
+    const jwtEncodedHeaders = jwtEncodedTokenList[0]
+    const jwtEncodedPayload = jwtEncodedTokenList[1]
+    const jwtEncodedSignature = jwtEncodedTokenList[2]
+    const jwtDecodedHeaders = Buffer.from(jwtEncodedHeaders, 'base64').toString('binary');
+    const jwtDecodedPayload = Buffer.from(jwtEncodedPayload, 'base64').toString('binary');
+    const jwtDecodedSignature = Buffer.from(jwtEncodedSignature, 'base64').toString('binary');
+
+    return (
+        <div>
+            <h1>JWT Headers</h1>
+            <pre>{jwtDecodedHeaders}</pre>
+
+            <h1>JWT Payload</h1>
+            <pre>{jwtDecodedPayload}</pre>
+
+            <h1>JWT Signature</h1>
+            <pre>{jwtDecodedSignature}</pre>
+        </div>
+    )
+}
