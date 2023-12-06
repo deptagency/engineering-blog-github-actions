1808fda4ef2cef7fbc47934d00bc693808369490
juttayaya <admin@apps.jirawat.com>
2023-12-06 06:13:51 -0500

    JWT

--- nextjs/hello-app/app/cognito-debug/page.tsx
+++ nextjs/hello-app/app/cognito-debug/page.tsx
@@ -1,39 +1,56 @@
 import { Buffer } from "buffer"
 import { headers } from 'next/headers'
+import crypto from 'crypto'
 
-export default function Page() {
-    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html    const headersList = headers()
+async function getJwtPayload() {
+    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html
 
     // Step 1: Get the key id from JWT headers (the kid field)
     const headersList = headers()
     const encodedJwt = headersList.get('x-amzn-oidc-data')
-
-    if (!encodedJwt) {
-        return (
-            <div>
-                <h1>No HTTP header for x-amzn-oidc-data</h1>
-            </div>
-        )
-    } else {
-
+    if (encodedJwt) {
+        const awsRegion = 'us-east-1'
         const jwtEncodedTokenList = encodedJwt.split('.')
         const jwtEncodedHeaders = jwtEncodedTokenList[0]
         const jwtEncodedPayload = jwtEncodedTokenList[1]
         const jwtEncodedSignature = jwtEncodedTokenList[2]
         const jwtDecodedHeaders = Buffer.from(jwtEncodedHeaders, 'base64').toString('binary');
-        const jwtDecodedPayload = Buffer.from(jwtEncodedPayload, 'base64').toString('binary');
-        const jwtDecodedSignature = Buffer.from(jwtEncodedSignature, 'base64').toString('binary');
+        const jwtPayload = Buffer.from(jwtEncodedPayload, 'base64').toString('binary');
 
+        // Step 2: Get the public key from regional endpoint
+        const jwtKid = JSON.parse(jwtDecodedHeaders).kid
+        const jwtPublicKeyUrl = `https://public-keys.auth.elb.${awsRegion}.amazonaws.com/${jwtKid}`
+        const jwtPublicKeyResponse = await fetch(jwtPublicKeyUrl, { method: 'GET' , cache: "no-store"})
+        const jwtPublicKey = await jwtPublicKeyResponse.text()
+
+        // Step 3: Verify the signature of the JWT using the public key
+        const verifier = crypto.createVerify('sha256')
+        verifier.update(`${jwtEncodedHeaders}.${jwtEncodedPayload}`)
+        const isVerified = verifier.verify(jwtPublicKey, jwtEncodedSignature, 'base64')
+        if (isVerified) {
+            return jwtPayload
+        } else {
+            return null
+        }
+    }
+}
+export default async function Page(){
+    const jwtPayload = await getJwtPayload()
+    if (!jwtPayload) {
         return (
             <div>
-                <h1>JWT Headers</h1>
-                <pre>{jwtDecodedHeaders}</pre>
-
+                <h1>No JWT Payload</h1>
+            </div>
+        )
+    } else {
+        const jwtUsername = JSON.parse(jwtPayload).username
+        return (
+            <div>
                 <h1>JWT Payload</h1>
-                <pre>{jwtDecodedPayload}</pre>
+                <pre>{jwtPayload}</pre>
 
-                <h1>JWT Signature</h1>
-                <pre>{jwtDecodedSignature}</pre>
+                <h1>JWT Username</h1>
+                <pre>{jwtUsername}</pre>
             </div>
         )
     }
